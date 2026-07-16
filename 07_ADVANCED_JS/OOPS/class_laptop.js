4b4101996b0f160237faa2ac827e70ddcfb7554e
sumitrajawat <rajawatsumit579@gmail.com>
2026-07-16 22:53:47 +0700

    Add class laptop in advance css

---
+++ 07_ADVANCED_JS/OOPS/class_laptop.js
@@ -0,0 +1,11 @@
+class laptop {
+    constructor(name,model,processor){
+        this.name = name;
+        this.model = model;
+        this.processor = processor;
+    }
+}
+
+let l1 = new laptop("victus","342987348927y","amd ryzen5");
+let l2 = new laptop("loq","34298734892re","amd ryzen7");
+let l3 = new laptop("tuf gaming","34298734823sd","intel core i8");
