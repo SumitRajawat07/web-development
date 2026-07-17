e93687299388278d3fd3a7d27aff12d5c85dc194
sumitrajawat <rajawatsumit579@gmail.com>
2026-07-17 21:56:30 +0700

    Add class bottle in advance css

---
+++ 07_ADVANCED_JS/OOPS/class_botttle.js
@@ -0,0 +1,12 @@
+class Bottle {
+    constructor() {
+        this.color = "red";
+        this.prize = 344;
+        this.company = "milton";
+    }
+    fillbottle(){};
+    drinkbottle(){};
+}
+
+let b1 = new Bottle();
+let b2 = new Bottle();
