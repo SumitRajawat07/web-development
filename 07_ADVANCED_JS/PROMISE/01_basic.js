7bc944b048f310f1bb552b2960c4cce3ff7285a3
sumitrajawat <rajawatsumit579@gmail.com>
2026-08-14 22:22:21 +0700

    Add basic in advance css

---
+++ 07_ADVANCED_JS/PROMISE/01_basic.js
@@ -0,0 +1,11 @@
+let order = new Promise((resolve, reject) => {
+
+    let success = true;
+
+    if (success) {
+        resolve("Order successful");
+    } else {
+        reject("Order failed");
+    }
+
+});
